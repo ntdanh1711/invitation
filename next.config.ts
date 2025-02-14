0444bd9aca8e8baadb3322c668765ca3f4742da1
ntdanh1711 <trongdanh.dev@gmail.com>
2025-02-14 16:08:19 +0700

    Initial commit: update config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enables the `app/` directory in Next.js 13+
-  },
 };
 
 export default nextConfig;
